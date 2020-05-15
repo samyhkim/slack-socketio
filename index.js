f0b5d1bd236d997674e736271e7ddcca1638ae30
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2020-05-14 19:24:31 -0700

    Setting up boilerplate for SocketIO Slack clone.

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const socketio = require("socket.io");
+
+let namespaces = require("./data/namespaces");
+console.log(namespaces);
+
+// loop through each namespace and listen for a connection
+namespaces.forEach((namespace) => {
+  console.log(namespace);
+});
+
+app.use(express.static(__dirname + "/public"));
+
+const expressServer = app.listen(8000);
+
+const io = socketio(expressServer);
+
+io.of("/").on("connection", (socket) => {
+  socket.emit("messageFromServer", { data: "Welcome to the socketio server" });
+  socket.on("messageToServer", (dataFromClient) => {
+    console.log(dataFromClient);
+  });
+  socket.join("level1");
+  socket
+    .to("level1")
+    .emit("joined", `${socket.id} says I have joined the level 1 room!`);
+});
+
+io.of("/admin").on("connection", (socket) => {
+  console.log("Someone connected to the admin namespace!");
+  io.of("/admin").emit("welcome", "Welcome to the admin channel.");
+});
